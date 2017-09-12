2567797e88ab4b5779f1d832cd1788198b759ebe
Prashant Mishra <mishraprashant1811998@gmail.com>
2017-09-12 21:57:34 +0530

    Create Candies.java

---
+++ Candies.java
@@ -0,0 +1,24 @@
+/*
+
+Given an integer n, return the largest number that contains exactly n digits.
+
+Example
+
+For n = 2, the output should be
+largestNumber(n) = 99.
+
+*/
+
+int largestNumber(int n) {
+    String res="";
+    for(int i=0;i<n;i++)
+        res=res+"9";
+    return Integer.parseInt(res);
+}
+
+
+*/
+
+int candies(int n, int m) {
+    return m-(m%n);
+}
