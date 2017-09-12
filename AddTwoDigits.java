3ab440e768cff489cc9d644b8d5e5f2fb02e70c6
Prashant Mishra <mishraprashant1811998@gmail.com>
2017-09-12 21:49:23 +0530

    Create AddTwoDigits.java

---
+++ AddTwoDigits.java
@@ -0,0 +1,14 @@
+/*
+
+You are given a two-digit integer n. Return the sum of its digits.
+
+Example
+
+For n = 29, the output should be
+addTwoDigits(n) = 11.
+
+*/
+
+int addTwoDigits(int n) {
+    return (n%10)+(n/10);
+}
