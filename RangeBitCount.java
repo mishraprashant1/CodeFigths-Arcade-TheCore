394b436f72bf1933f098f959809ebd776793db42
Prashant Mishra <mishraprashant1811998@gmail.com>
2017-09-13 23:02:05 +0530

    Create RangeBitCount.java

---
+++ RangeBitCount.java
@@ -0,0 +1,25 @@
+/*
+
+You are given two numbers a and b where 0 ≤ a ≤ b. Imagine you construct an array of all the integers from a to b inclusive. You need to count the number of 1s in the binary representations of all the numbers in the array.
+
+Example
+
+For a = 2 and b = 7, the output should be
+rangeBitCount(a, b) = 11.
+
+Given a = 2 and b = 7 the array is: [2, 3, 4, 5, 6, 7]. Converting the numbers to binary, we get [10, 11, 100, 101, 110, 111], which contains 1 + 2 + 1 + 2 + 2 + 3 = 11 1s.
+
+*/
+
+int rangeBitCount(int a, int b) {
+    String temp="";
+    int count=0;
+    for(int i=a;i<=b;i++){
+        temp=Integer.toBinaryString(i);
+        for(int j=0;j<temp.length();j++){
+            if(temp.charAt(j)=='1')
+                count++;
+        }
+    }
+    return count;
+}
